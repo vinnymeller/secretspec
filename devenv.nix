1ce711474c6af6f903b175efbdcce8e55c028cee
Domen Kožar <domen@cachix.org>
2025-07-07 11:58:43 -0500

    add ci

--- devenv.nix
+++ devenv.nix
@@ -1,3 +1,13 @@
 {
   languages.rust.enable = true;
+
+  pre-commit.hooks = {
+    cargo-check.enable = true;
+    rustfmt.enable = true;
+    clippy.enable = true;
+  };
+
+  enterTest = ''
+    cargo test --all --verbose
+  '';
 }
